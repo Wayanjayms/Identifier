9737b0a634eb2bf00732a29894f9b31c8f0f063b
Wayanjayms <134066381+Wayanjayms@users.noreply.github.com>
2023-05-20 10:19:03 +0800

    finalproj.py

--- finalproj.py
+++ finalproj.py
@@ -1,11 +1,11 @@
 import streamlit as st
-
+import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model(os.path.join('models','Dog_or_Cat_classifier.h5'))
+  model=tf.keras.models.load_model(os.path.join('/content/drive/MyDrive/Models','Dog_or_Cat_classifier.h5'))
   return model
-model=tf.keras.models.load_model(os.path.join('models','Dog_or_Cat_classifier.h5'))
+model=tf.keras.models.load_model(os.path.join('/content/drive/MyDrive/Models','Dog_or_Cat_classifier.h5'))
 st.write("""
 # Dog or cat classifier"""
 )
