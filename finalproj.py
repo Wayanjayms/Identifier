373495ba92c7971d5de75caaaa2dd75c23e0b00a
Wayanjayms <134066381+Wayanjayms@users.noreply.github.com>
2023-05-25 19:57:10 +0800

    Update finalproj.py

--- finalproj.py
+++ finalproj.py
@@ -3,13 +3,13 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-  model=tf.keras.models.load_model('weather_classification.h5')
+  model=tf.keras.models.load_model('weather_classifier.h5')
   return model
 model=load_model()
 st.write("""
-# Weather Classification"""
+# Weather Classifier"""
 )
-file=st.file_uploader("Upload a weather photo from your computer.",type=["jpg","png"])
+file=st.file_uploader("Upload a weather Atmosphere photo.",type=["jpg","png"])
 
 import cv2
 from PIL import Image,ImageOps
